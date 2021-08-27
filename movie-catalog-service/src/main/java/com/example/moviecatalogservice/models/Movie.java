4658d1a4cafe0ef455ef822a1753d185726cb1b9
Shubha2207 <shubha.pawar2207@gmail.com>
2021-08-27 18:57:55 +0530

    added restTemplate to movie-catalog...make it communicate to movie-info service

---
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/models/Movie.java
@@ -0,0 +1,32 @@
+package com.example.moviecatalogservice.models;
+
+public class Movie {
+
+    private String movieId;
+    private String description;
+
+    public Movie(){
+
+    }
+
+    public Movie(String movieId, String description) {
+        this.movieId = movieId;
+        this.description = description;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,24 +1,45 @@
 package com.example.moviecatalogservice.resources;
 
 import com.example.moviecatalogservice.models.CatalogItem;
+import com.example.moviecatalogservice.models.Movie;
+import com.example.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalogItems(@PathVariable("userId") String userId) {
-//        return "List of movie ratings of "+userId;
-        return Collections.singletonList(
-          new CatalogItem("transformer", "sci-fi movie", 4)
+
+        // RestTemplate allows us to make rest call to another api
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("AvengerIW", 4),
+                new Rating("AvengerEG", 5)
         );
+
+
+        // iterating through list of ratings
+        return ratings.stream().map(rating -> {
+            // getForObject method unmarshell response from string to java object
+            // must include empty constructor in the movie class
+            // otherwise it wont be able to create an object of that class and copy data into it
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(),Movie.class);
+            return new CatalogItem(rating.getMovieId(), movie.getDescription(), rating.getRating());
+        }).collect(Collectors.toList());
+
     }
 
 }
